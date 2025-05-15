9421d0cfb1c027cb27dcfce153853205c2384d91
itsmebalu221 <154417002+itsmebalu221@users.noreply.github.com>
2025-05-15 11:55:29 +0530

    logs

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use(express.json());
